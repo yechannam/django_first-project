3563423b187a1876a6f588000dac7ec49e2726c4
Nam Yechan <chanmias0318@gmail.com>
2023-03-15 12:16:46 +0900

    Update templates

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,22 @@
-from django.http import HttpResponse as http
+from django.http import HttpResponse
+from django.template import loader
+from .models import Question
 
-def index(reauest):
-	return http("Hello World")
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_data')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+		'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+	
