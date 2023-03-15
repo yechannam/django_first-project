3afccbe53a8b46cb48ee933f4338f6be2645cfbc
Nam Yechan <chanmias0318@gmail.com>
2023-03-15 19:22:45 +0900

    Update test

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,9 +1,11 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_data = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published')
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
